ceb1ed3a5ceda891374bf72ab78b882442c70cf2
Alexandru Novak <alexandru.novak@visma.com>
2023-03-22 14:16:46 +0200

    let's see what master ADI did

--- dms-test.py
+++ dms-test.py
@@ -3,7 +3,7 @@ import json
 import os
 import subprocess
 import yaml
-
+import boto3
 
 # Define the replication task settings
 rep_task_settings = {
@@ -15,58 +15,80 @@ rep_task_settings = {
     'table-mappings': '',
 }
 
-# Load the table names from the YAML file
-with open('table_names.yml', 'r') as file:
-    table_names = yaml.safe_load(file)
+# Load the table names from the file
+table_names_file = open('table_names', 'r')
+table_names=table_names_file.readlines()
 
 # Create the table mappings for each table
-table_mappings = {
-    'rules': []
-}
+client = boto3.client('dms')
 for table_name in table_names:
-    table_mapping = {
-        'rule-type': 'selection',
-        'rule-id': 2,
-        'rule-name': 2,
-        'object-locator': {
-            'schema-name': 'tripletex',
-            'table-name': table_name
-        },
-        'rule-action': 'include'
-    }
-    table_mappings['rules'].append(table_mapping)
+  table_mappings = {
+    'rules': []
+  }
+  table_mapping = {
+      'rule-type': 'selection',
+      'rule-id': '2',
+      'rule-name': '2',
+      'object-locator': {
+          'schema-name': 'tripletex',
+          'table-name': table_name.strip()
+      },
+      'rule-action': 'include'
+  }
+  table_mappings['rules'].append(table_mapping)
 
-# Add the schema renaming rule to the table mappings
-schema_rename_rule = {
-    'rule-type': 'transformation',
-    'rule-id': '1',
-    'rule-name': '1',
-    'rule-target': 'schema',
-    'object-locator': {
-        'schema-name': 'tripletex'
-    },
-    'rule-action': 'rename',
-    'value': 'globaldata',
-    'old-value': None
-}
-table_mappings['rules'].insert(0, schema_rename_rule)
+  # Add the schema renaming rule to the table mappings
+  schema_rename_rule = {
+      'rule-type': 'transformation',
+      'rule-id': '1',
+      'rule-name': '1',
+      'rule-target': 'schema',
+      'object-locator': {
+          'schema-name': 'tripletex'
+      },
+      'rule-action': 'rename',
+      'value': 'globaldata',
+      'old-value': None
+  }
+  table_mappings['rules'].insert(0, schema_rename_rule)
 
-# Add the table mappings to the replication task settings
-rep_task_settings['table-mappings'] = json.dumps(table_mappings)
+  # Add the table mappings to the replication task settings
+  rep_task_settings['table-mappings'] = json.dumps(table_mappings)
 
-# Create the replication task using AWS CLI
-cmd = ['aws', 'dms', 'create-replication-task']
-for key, value in rep_task_settings.items():
-    cmd.extend(['--'+key, value])
+  task_settings = {
+      'FullLoadSettings': {
+          'TargetTablePrepMode': 'DO_NOTHING'
+      },
+      'ValidationSettings': {
+          'ValidationPartialLobSize': 0,
+          'PartitionSize': 10000,
+          'RecordFailureDelayLimitInMinutes': 0,
+          'SkipLobColumns': False,
+          'FailureMaxCount': 10000,
+          'HandleCollationDiff': False,
+          'ValidationQueryCdcDelaySeconds': 0,
+          'ValidationMode': 'ROW_LEVEL',
+          'TableFailureMaxCount': 1000,
+          'RecordFailureDelayInMinutes': 5,
+          'MaxKeyColumnSize': 8096,
+          'EnableValidation': True,
+          'ThreadCount': 5,
+          'RecordSuspendDelayInMinutes': 30,
+          'ValidationOnly': False
+      }
+  }
 
-
-if __name__ == "__main__":
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument('--noop', action='store_true')
-
-    args = argParser.parse_args()
-    if args.noop:
-        print(cmd)
-    else:
-        subprocess.run(cmd)
+  # Create replication task
+  try:
+    response = client.create_replication_task(
+        ReplicationTaskIdentifier=table_name.strip()+'-to-globaldata',
+        SourceEndpointArn=rep_task_settings['source-endpoint-arn'],
+        TargetEndpointArn=rep_task_settings['target-endpoint-arn'],
+        ReplicationInstanceArn=rep_task_settings['replication-instance-arn'],
+        MigrationType='full-load-and-cdc',
+        TableMappings=rep_task_settings['table-mappings'],
+        ReplicationTaskSettings=json.dumps(task_settings)
+    )
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
